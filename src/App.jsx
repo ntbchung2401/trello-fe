1e08c8c4c20b737296c7b011396cf9d4fffb726b
nchungojt07 <chung.nguyen-ojt07@devplus.edu.vn>
2025-01-13 23:09:34 +0700

    Test dark light mode theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,61 @@
 import Button from '@mui/material/Button'
 import HomeIcon from '@mui/icons-material/Home'
-import { pink } from '@mui/material/colors'
+import { useColorScheme } from '@mui/material'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeIcon from '@mui/icons-material/DarkMode'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+import Box from '@mui/material/Box'
+
+function SelectMode() {
+  const { mode, setMode } = useColorScheme()
+  const handleChange = (event) => {
+    setMode(event.target.value)
+  }
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-mode"
+        id="select-light-dark-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="system">
+          <Box sx={{ display: 'flex', gap: 2, alignItems: 'center' }}>
+            <SettingsBrightnessIcon fontSize='small' /> System
+          </Box>
+        </MenuItem>
+        <MenuItem value="dark">
+          <Box sx={{ display: 'flex', gap: 2, alignItems: 'center' }}>
+            <DarkModeIcon fontSize='small' /> Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value="light">
+          <Box sx={{ display: 'flex', gap: 2, alignItems: 'center' }}>
+            <LightModeIcon fontSize='small' /> Light
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
 
 function App() {
   return (
     <>
-      <HomeIcon color="primary" />
+      <SelectMode/>
       <br />
-      <Button variant="contained">Test Button</Button>
+      <HomeIcon color='text.secondary' />
+      <br />
+      <Button variant='contained' color='success'>
+        Test Button
+      </Button>
     </>
   )
 }
